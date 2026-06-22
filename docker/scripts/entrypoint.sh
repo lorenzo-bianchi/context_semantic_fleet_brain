af21b2db9f87add47489a1aa54ec1454dceb69b1
Lorenzo Bianchi <lnz.bnc@gmail.com>
2026-06-22 18:18:40 +0200

    chore: use ROS 2 Jazzy instead of Lyrical

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-source "/opt/ros/lyrical/setup.bash"
+source "/opt/ros/jazzy/setup.bash"
 
 if [ -f "/workspace/install/setup.bash" ]; then
     source "/workspace/install/setup.bash"
